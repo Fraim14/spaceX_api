6e6015ad19b9e0e5d69b605f24e954737c08860e
Maksym Shtymak <maksym.shtymak@student.griffith.ie>
2025-04-14 00:03:56 +0100

    add post methot to apiCalls file

--- apiCalls.java
+++ apiCalls.java
@@ -1,7 +1,9 @@
+
 import org.json.JSONObject;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.io.OutputStream;
 import java.net.URL;
 import java.net.HttpURLConnection;
 import java.io.InputStream;
@@ -9,10 +11,11 @@ import java.io.InputStream;
 
 public class apiCalls {
     private final String baseUrl = "https://api.spacexdata.com/v4";
+    public URL url;
 
     //this method is used to make a GET request to the SpaceX API
     public JSONObject get(String endpoint) throws Exception {
-        URL url = new URL(baseUrl + endpoint);
+        url = new URL(baseUrl + endpoint);
         HttpURLConnection connection = (HttpURLConnection) url.openConnection();
         connection.setRequestMethod("GET");
 
@@ -28,6 +31,40 @@ public class apiCalls {
             reader.close();
             inputStream.close();
             connection.disconnect();
+            return new JSONObject(response.toString());
+
+        } finally {
+            connection.disconnect();
+        }
+
+
+    }
+
+    //this method is used to make a POST request to the SpaceX API to filter the input data by the user
+    public JSONObject post(String endpoint, JSONObject filter) throws Exception {
+        url = new URL(baseUrl + endpoint);
+        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+        connection.setRequestMethod("POST");
+
+
+        try {
+            try {
+                OutputStream outputStream = connection.getOutputStream();
+                byte[] input = filter.toString().getBytes();
+                outputStream.write(input, 0, input.length);
+                outputStream.close();
+            } catch (Exception e) {
+                System.out.println(e);
+            }
+            InputStream inputStream = connection.getInputStream();
+            BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+            StringBuilder response = new StringBuilder();
+            String line;
+            while ((line = reader.readLine()) != null) {
+                response.append(line);
+            }
+            reader.close();
+            inputStream.close();
             return new JSONObject(response.toString());
 
         } finally {
