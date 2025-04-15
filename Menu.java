438c9b0552ab7df50c41a0c143b95fd72f2d0b97
Maksym Shtymak <maksym.shtymak@student.griffith.ie>
2025-04-15 18:45:56 +0100

    create the menu and its display

---
+++ Menu.java
@@ -0,0 +1,83 @@
+import java.util.Scanner;
+
+public class Menu {
+    public Scanner scanner;
+    Filter filter = new Filter();
+
+    public Menu() {
+        scanner = new Scanner(System.in);
+    }
+
+    public void menuOptions() {
+
+        System.out.println("Welcome to the Space API!");
+        System.out.println("Please select an option:");
+        System.out.println("1.  Select what you want to find");
+        System.out.println("2. Settings");
+        System.out.println("3. Cache");
+        System.out.println("4. Exit");
+    }
+
+    public void castMenu() {
+        while (true) {
+            String input = scanner.nextLine();
+            switch (input) {
+                case "1":
+                    displayFilterMenu();
+                    castFilterMenu();
+                    break;
+                case "2":
+                    System.out.println("You have selected settings...");
+                    break;
+                case "3":
+                    System.out.println("You have selected cache...");
+                    break;
+                case "4":
+                    System.out.println("You have selected exit...");
+                    break;
+                default:
+                    System.out.println("Invalid input. Please try again.");
+                    menuOptions();
+                    break;
+
+            }
+        }
+    }
+
+    public void displayFilterMenu() {
+        System.out.println("Please select an option:");
+        System.out.println("1. Filter");
+        System.out.println("2. Find by ID");
+        System.out.println("3. Exit");
+    }
+
+    public void castFilterMenu() {
+        while (true) {
+            String input = scanner.nextLine();
+            switch (input) {
+                case "1":
+                    filter.filterData();
+                    break;
+                case "2":
+                    filter.findById();
+                    break;
+                case "3":
+                    System.out.println("You have left the data search menu...");
+                    displayMenu();
+                    break;
+                default:
+                    System.out.println("Invalid input. Please try again.");
+                    displayFilterMenu();
+                    break;
+            }
+        }
+
+    }
+
+    public void displayMenu() {
+        menuOptions();
+        castMenu();
+    }
+
+
+}
